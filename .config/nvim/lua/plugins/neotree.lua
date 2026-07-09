0a9c482367926c491f8e674738f1dc04637dc849
aukvary <al3xpop123@gmail.com>
2026-07-09 22:07:02 +0700

    nvim keys

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,34 @@
 vim.pack.add({
     { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range('3') },
-    { src = "https://github.com/nvim-lua/plenary.nvim"},
-    { src = "https://github.com/MunifTanjim/nui.nvim"},
-    { src = "https://github.com/nvim-tree/nvim-web-devicons"},
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/MunifTanjim/nui.nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
+
+require("neo-tree").setup({
+    source_selector = {
+        winbar = false,
+        statusline = false
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            show_hidden_count = false,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {},
+            never_show = {},
+        },
+    },
+    window = {
+        mappings = {
+            ["<space>"] = "none",
+            ["l"] = "open",
+            ["h"] = "close_node",
+            ["v"] = "open_vsplit",
+            ["s"] = "open_split",
+            ["t"] = "open_tabnew",
+            ["P"] = { "toggle_preview", config = { use_float = true } },
+        }
+    }
 })
